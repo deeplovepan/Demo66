c0bec87f5f794d4d4d8e8a2676006174eacbee1e
deeplovepan <deeplovepan@gmail.com>
2017-07-19 16:19:24 +0800

    Initial Commit

---
+++ Demo6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo6
+//
+//  Created by Peter Pan on 19/07/2017.
+//  Copyright © 2017 Peter Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
